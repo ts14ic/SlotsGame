14754d760a6c00499c6715c46bd54c5d06ddf416
Artyom Yazinsky <iw2snl@gmail.com>
2018-08-28 00:58:32 +0300

    Add some fancy to the FoundLine toString

--- src/main/java/md/ts14ic/slotsgame/slots/FoundLine.java
+++ src/main/java/md/ts14ic/slotsgame/slots/FoundLine.java
@@ -15,11 +15,25 @@ public class FoundLine {
         return payout;
     }
 
+    /**
+     * Return a string representation in the following form:
+     * <pre>
+     *     [ |{01}234|, length: 2, payout: 300 ]
+     * </pre>
+     * 1. The whole string is enclosed in [] brackets
+     * 2. A line enclosed in || brackets is the first element
+     * 3. The || line includes a {} line, which is the fragment that brought the payout
+     * 4. The length of that fragment is the second element
+     * 5. The payout is also listed
+     */
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder("[ |");
+        StringBuilder builder = new StringBuilder("[ |{");
         for (int i = 0; i < ruleLine.length(); ++i) {
             builder.append(ruleLine.cell(i));
+            if (i == length - 1) {
+                builder.append("}");
+            }
         }
         return builder.append("|, length: ")
                 .append(length)
