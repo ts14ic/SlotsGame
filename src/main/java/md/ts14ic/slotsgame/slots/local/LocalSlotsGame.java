82751fbbf8bc017467c0b6be3538338ebc5a97c8
Artyom Yazinsky <iw2snl@gmail.com>
2018-08-27 22:02:17 +0300

    Make payouts map unmodifiable

--- src/main/java/md/ts14ic/slotsgame/slots/local/LocalSlotsGame.java
+++ src/main/java/md/ts14ic/slotsgame/slots/local/LocalSlotsGame.java
@@ -10,30 +10,32 @@ import java.util.*;
 public class LocalSlotsGame implements SlotsGame {
     private static final int ROWS_COUNT = 3;
     private static final int COLUMNS_COUNT = 5;
-    private static final Map<Rule, Integer> mPayouts = new HashMap<>();
+    private static final Map<Rule, Integer> PAYOUTS;
 
     private List<Payline> mPaylines = new ArrayList<>();
     private SlotsGame.Listener mListener;
     private int mTotalPayout = 0;
 
     static {
-        mPayouts.put(new Rule(LocalSlots.SLOTS[0], 2), 40);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[0], 3), 75);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[0], 4), 200);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[0], 5), 750);
+        Map<Rule, Integer> payouts = new HashMap<>();
+        payouts.put(new Rule(LocalSlots.SLOTS[0], 2), 40);
+        payouts.put(new Rule(LocalSlots.SLOTS[0], 3), 75);
+        payouts.put(new Rule(LocalSlots.SLOTS[0], 4), 200);
+        payouts.put(new Rule(LocalSlots.SLOTS[0], 5), 750);
 
-        mPayouts.put(new Rule(LocalSlots.SLOTS[1], 2), 3);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[1], 3), 10);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[1], 4), 30);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[1], 5), 40);
+        payouts.put(new Rule(LocalSlots.SLOTS[1], 2), 3);
+        payouts.put(new Rule(LocalSlots.SLOTS[1], 3), 10);
+        payouts.put(new Rule(LocalSlots.SLOTS[1], 4), 30);
+        payouts.put(new Rule(LocalSlots.SLOTS[1], 5), 40);
 
-        mPayouts.put(new Rule(LocalSlots.SLOTS[2], 3), 10);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[2], 4), 10);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[2], 5), 100);
+        payouts.put(new Rule(LocalSlots.SLOTS[2], 3), 10);
+        payouts.put(new Rule(LocalSlots.SLOTS[2], 4), 10);
+        payouts.put(new Rule(LocalSlots.SLOTS[2], 5), 100);
 
-        mPayouts.put(new Rule(LocalSlots.SLOTS[3], 3), 30);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[3], 4), 100);
-        mPayouts.put(new Rule(LocalSlots.SLOTS[3], 5), 500);
+        payouts.put(new Rule(LocalSlots.SLOTS[3], 3), 30);
+        payouts.put(new Rule(LocalSlots.SLOTS[3], 4), 100);
+        payouts.put(new Rule(LocalSlots.SLOTS[3], 5), 500);
+        PAYOUTS = Collections.unmodifiableMap(payouts);
     }
 
     public LocalSlotsGame(SlotsGame.Listener listener) {
@@ -85,8 +87,8 @@ public class LocalSlotsGame implements SlotsGame {
 
         Rule rule = new Rule(startingSlot, length);
 
-        if (mPayouts.containsKey(rule)) {
-            int payout = mPayouts.get(rule) * bet;
+        if (PAYOUTS.containsKey(rule)) {
+            int payout = PAYOUTS.get(rule) * bet;
             mTotalPayout += payout;
             mPaylines.add(payline);
             mListener.onLineFound(payline, length, payout);
